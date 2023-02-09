02a7093984406d030caf331bb1909d4fc92182ae
Sergei Pleshanov <2357qwr@gmail.com>
2023-02-09 23:24:54 +0300

    feat: add the starting configuration of the webpack + TS

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import test from './test';
+
+test(3);
+
+document.body.innerHTML = `<div> HELLO!!!! </div>`;
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export default function fn(arg: number): string {
+  console.log('RANDOM TEXT')
+  return 'tartatt';
+}
