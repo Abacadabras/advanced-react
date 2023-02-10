420e3a5ca62f9cdba3e61890bd1a88d5b7fbd2f1
Sergei Pleshanov <2357qwr@gmail.com>
2023-02-10 15:50:06 +0300

    feat: add react and scss loader, update test fails (counter app react)

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,13 +2,22 @@ import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
 
-  const typeScriptLoader =  {
+  const typeScriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   };
 
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      "style-loader",
+      "css-loader",
+      "sass-loader",
+    ],
+  }
   return [
-   typeScriptLoader,
+    typeScriptLoader,
+    cssLoader,
   ];
 }
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export default function fn(arg: number): string {
-  console.log('RANDOM TEXT')
-  return 'tartatt';
-}
